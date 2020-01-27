ff274be00354667b2e6b51b564dfb38b058da977
AmandaNiy23 <amandaniyonkuru@cmail.carleton.ca>
2020-01-27 00:25:38 -0500

    Updated settings

--- social_app/settings.py
+++ social_app/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#u@x&irhnyt_1d=zyj503yyj0*%5!1#*)_28ocbht01def0@b0'
+#SECRET_KEY = '#u@x&irhnyt_1d=zyj503yyj0*%5!1#*)_28ocbht01def0@b0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yspk.herokuapp.com']
 
 
 # Application definition
